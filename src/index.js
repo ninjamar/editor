9545603ad08267c186ee7016f06611895bcc7ca3
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-04-14 08:41:55 +0800

    Add text centering + resolve #25

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import { Editor } from "./editor/editor.js";
 import dedent from 'https://cdn.jsdelivr.net/npm/dedent@1.5.3/+esm';
 
-/*let contents = btoa(`This div is editable
-Another line
-This <span style="font-style: italic;">word</span> is already italic
-`);*/
 let contents = dedent`
-    This div is editable
-    Another line
+    This div is editable <br>
+    Another line <br>
     This <span style="font-style: italic;">word</span> is already italic
 `;
 document.addEventListener("DOMContentLoaded", () => {
