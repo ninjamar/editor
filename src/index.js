5fab55d4b81b52e1453f856e2379afa1eab0a1bf
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-04-14 19:24:08 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { Editor } from "./editor/editor.js";
 import dedent from 'https://cdn.jsdelivr.net/npm/dedent@1.5.3/+esm';
 
 let contents = dedent`
-    This div is editable <br>
-    Another line <br>
+    This div is editable
+    Another line
     This <span style="font-style: italic;">word</span> is already italic
 `;
 document.addEventListener("DOMContentLoaded", () => {
